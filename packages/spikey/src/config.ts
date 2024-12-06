af5aa0faf2b8e694b063679644032323dceecb5c
Kevin Zoltany <kzoltany@gmail.com>
2024-12-06 00:57:38 -0800

    Initial Commit

---
+++ packages/spikey/src/config.ts
@@ -0,0 +1,40 @@
+const defaultConfig: SpikeyConfig = {
+    pluginName: 'My StreamDeck Plugin',
+    version: '1.0.0',
+    author: 'Your Name',
+    pluginUUID: 'com.yourname.myplugin',
+    supportedDevices: ['StreamDeck', 'StreamDeckMini', 'StreamDeckXL'],
+    actions: {},
+}
+
+export class SpikeyConfigManager {
+    private config: SpikeyConfig
+
+    constructor(user_config?: Partial<SpikeyConfig>) {
+        this.config = {
+            ...defaultConfig,
+            ...user_config,
+
+            actions: {
+                ...defaultConfig.actions,
+                ...(user_config?.actions ?? {}),
+            },
+        }
+    }
+
+    validate(): boolean {
+        if (!this.config.pluginUUID) {
+            console.warn('No plugin UUID found in config, using default value')
+        }
+
+        return true
+    }
+
+    get_config(): SpikeyConfig {
+        return this.config
+    }
+
+    get_action_config(action_id: string) {
+        return this.config.actions[action_id]
+    }
+}
